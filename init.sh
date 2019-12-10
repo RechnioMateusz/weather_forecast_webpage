6007028d37ddf7d1668c1c4111ca7ca83ac44080
RechnioMateusz <mateusz.rechnio@op.pl>
2019-12-10 20:57:37 +0100

    Added some proper ASCII art in bash scripts

--- init.sh
+++ init.sh
@@ -1,12 +1,25 @@
+# Path to python requirements file
 REQS=python_requirements.txt
+
+# Path manage.py
 MANAGE=weather_forecast/manage.py
 
+# ASCII art by 'jgs' from https://ascii.co.uk/art
+echo "      ,"
+echo "     /(  ___________"
+echo "    |  >:===========\`        Working on initialization"
+echo "     )("
+echo "     \"\""
+
+
+# If manage.py exists run server, echo otherwise
 if test -f "$REQS"; then
     pip install -r "$REQS"
 else
     echo "$REQS DOES NOT EXIST!"
 fi
 
+# If manage.py exists makemigrations, migrate, create super user, echo otherwise
 if test -f "$MANAGE"; then
     python "$MANAGE" makemigrations
     python "$MANAGE" migrate
