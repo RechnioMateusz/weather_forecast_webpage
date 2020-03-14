a0e4ec15f9d9780df1566ef1ba655d19207295bd
Tomek Wójcik <vooydzig@gmail.com>
2020-03-14 22:47:46 +0100

    added custom unix timestamp serializer field

--- weather_forecast/device/serializers.py
+++ weather_forecast/device/serializers.py
@@ -1,7 +1,15 @@
-from rest_framework import serializers
+from datetime import datetime
 
+from rest_framework import serializers
 from .models import Device, Data
 
+class UnixTimestampField(serializers.Field):
+    def to_representation(self, value):
+        epoch = datetime(1970, 1, 1)
+        return (value - epoch).total_seconds()
+
+    def to_internal_value(self, data):
+        return datetime.utcfromtimestamp(data)
 
 class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +18,8 @@ class DeviceSerializer(serializers.ModelSerializer):
 
 
 class DataSerializer(serializers.ModelSerializer):
+    datetime = UnixTimestampField()
+
     class Meta:
         model = Data
         fields = '__all__'
